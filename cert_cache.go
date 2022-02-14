e3792675e62fe181f2ab706d67d12aa57bc8e450
Lekan Adigun <adigunhammed.lekan@gmail.com>
2022-02-14 13:11:41 +0000

    recv database handle

--- cert_cache.go
+++ cert_cache.go
@@ -2,9 +2,7 @@ package autocertcache
 
 import (
 	"context"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"gorm.io/gorm"
 	"sync"
 )
 
